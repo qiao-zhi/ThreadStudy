6ccaa5ef69ddbe4e9bcc004d5f03f8406ec0a411
qiaozhi <qiao_liqiang@163.com>
2018-12-04 22:07:05 +0800

    init

---
+++ src/cn/qlq/thread/one/SubThread.java
@@ -0,0 +1,20 @@
+package cn.qlq.thread.one;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SubThread extends Thread {
+
+	private static final Logger log = LoggerFactory.getLogger(SubThread.class);
+
+	@Override
+	public void run() {
+		log.debug("subThread run,threadname->", Thread.currentThread().getName());
+	}
+
+	public static void main(String[] args) {
+		SubThread subThread = new SubThread();
+		subThread.start();
+		log.debug("运行结束,threadname->{}", Thread.currentThread().getName());
+	}
+}
---
+++ src/cn/qlq/thread/one/Test1.java
@@ -0,0 +1,7 @@
+package cn.qlq.thread.one;
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println(Thread.currentThread().getName());
+	}
+}
