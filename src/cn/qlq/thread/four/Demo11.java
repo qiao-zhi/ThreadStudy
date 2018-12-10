f38b1778eacf7fdc0d39690de33ad31b2feef8c1
qiaozhi <qiao_liqiang@163.com>
2018-12-10 22:08:34 +0800

    xx

--- src/cn/qlq/thread/four/Demo11.java
+++ src/cn/qlq/thread/four/Demo11.java
@@ -3,8 +3,6 @@ package cn.qlq.thread.four;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sun.swing.internal.plaf.synth.resources.synth;
-
 public class Demo11 extends Thread {
 
 	public static void main(String[] args) throws InterruptedException {
