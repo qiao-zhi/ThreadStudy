60de181ca6ad879724968239911e8c1328e85ab5
qiao_zhi <qiao_liqiang@163.com>
2019-01-03 19:24:39 +0800

    xx

---
+++ src/cn/qlq/thread/eighteen/Demo1.java
@@ -0,0 +1,55 @@
+package cn.qlq.thread.eighteen;
+
+import java.util.Vector;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cn.qlq.thread.seventeen.Demo5;
+
+public class Demo1 {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Demo5.class);
+
+	public static void main(String[] args) throws InterruptedException {
+		final Vector<String> vector = new Vector<>();
+		vector.add("11111111");// 添加元素
+
+		Thread.sleep(1 * 1000);
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Demo1.getLast(vector);
+			}
+		}).start();
+		// 删除元素
+
+		Thread.sleep(1 * 100);
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Demo1.removeLast(vector);
+			}
+		}).start();
+
+	}
+
+	public static Object getLast(Vector<String> list) {
+		int index = list.size() - 1;
+		LOGGER.info("getLast:index ->{}", index);
+		// 休眠等待第二个线程删除
+		try {
+			Thread.sleep(5 * 100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return list.get(index);
+	}
+
+	public static void removeLast(Vector<String> list) {
+		int index = list.size() - 1;
+		list.remove(index);
+		LOGGER.info("成功删除元素,index{},threadName->{}", index, Thread.currentThread().getName());
+	}
+
+}
