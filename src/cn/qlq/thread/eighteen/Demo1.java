ab5769a9af190a006e0ad3cea58f3199fcb9b12a
qiaozhi <qiao_liqiang@163.com>
2019-01-03 22:23:30 +0800

    浣ゅ北

--- src/cn/qlq/thread/eighteen/Demo1.java
+++ src/cn/qlq/thread/eighteen/Demo1.java
@@ -12,6 +12,14 @@ public class Demo1 {
 
 	public static void main(String[] args) throws InterruptedException {
 		final Vector<String> vector = new Vector<>();
+
+		synchronized(vector){
+			for (int i = 0; i < vector.size(); i++) {
+				System.out.println(vector.get(i));
+			}
+		}
+
+
 		vector.add("11111111");// 添加元素
 
 		Thread.sleep(1 * 1000);
@@ -35,21 +43,25 @@ public class Demo1 {
 	}
 
 	public static Object getLast(Vector<String> list) {
-		int index = list.size() - 1;
-		LOGGER.info("getLast:index ->{}", index);
-		// 休眠等待第二个线程删除
-		try {
-			Thread.sleep(5 * 100);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		synchronized (list) {
+			int index = list.size() - 1;
+			LOGGER.info("getLast:index ->{}", index);
+			// 休眠等待第二个线程删除
+			try {
+				Thread.sleep(5 * 100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return list.get(index);
 		}
-		return list.get(index);
+
 	}
 
-	public static void removeLast(Vector<String> list) {
-		int index = list.size() - 1;
-		list.remove(index);
-		LOGGER.info("成功删除元素,index{},threadName->{}", index, Thread.currentThread().getName());
+	public static synchronized void removeLast(Vector<String> list) {
+		synchronized (list) {
+			int index = list.size() - 1;
+			list.remove(index);
+			LOGGER.info("成功删除元素,index{},threadName->{}", index, Thread.currentThread().getName());
+		}
 	}
-
 }
