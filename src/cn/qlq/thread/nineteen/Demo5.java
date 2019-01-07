aeee3c5863bdca739e084291c36c1833194f01ab
qiao_zhi <qiao_liqiang@163.com>
2019-01-07 19:21:21 +0800

    完善CAS

--- src/cn/qlq/thread/nineteen/Demo5.java
+++ src/cn/qlq/thread/nineteen/Demo5.java
@@ -1,22 +1,30 @@
 package cn.qlq.thread.nineteen;
 
 import java.lang.reflect.Field;
-
-import com.sun.swing.internal.plaf.basic.resources.basic;
+import java.util.Arrays;
 
 import sun.misc.Unsafe;
 
 public class Demo5 {
 	public static void main(String[] args) throws Exception {
 		int[] array = new int[] { 1, 2, 3 };
+		System.out.println(Arrays.toString(array));
 		Unsafe unsafeInstance = getUnsafeInstance();
-		int arrayBaseOffset = unsafeInstance.arrayBaseOffset(int[].class);// 获取基地址
+		// 获取数组的基地址
+		int arrayBaseOffset = unsafeInstance.arrayBaseOffset(int[].class);
 		System.out.println(arrayBaseOffset);
 
-		int scale = unsafeInstance.arrayIndexScale(int[].class);// 比例值
-		int shift = 31 - Integer.numberOfLeadingZeros(scale);
-		long offset = ((long) 1 << shift) + arrayBaseOffset;
-		System.out.println(unsafeInstance.getInt(offset));
+		// 比例值--每个元素占的大小
+		int scale = unsafeInstance.arrayIndexScale(int[].class);
+		System.out.println(scale);
+
+		// 修改index为1元素的值为22
+		unsafeInstance.putInt(array, arrayBaseOffset + 1 * scale, 22);
+
+		// 采用元素加地址偏移量或者元素的值
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(unsafeInstance.getInt(array, arrayBaseOffset + i * scale));
+		}
 	}
 
 	// 反射获取unsafe实例
